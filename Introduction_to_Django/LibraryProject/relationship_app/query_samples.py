c2c9e653813a40a6f7e99470078253f7e3d30bce
MinaBG96 <minaboles96@gmail.com>
2025-11-11 12:02:48 +0200

    Advanced Model Relationships in Django v1.10

--- Introduction_to_Django/LibraryProject/relationship_app/query_samples.py
+++ Introduction_to_Django/LibraryProject/relationship_app/query_samples.py
@@ -11,13 +11,15 @@ def books_by_author(author_name):
 # List all books in a library
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    books = library.books.all()
+    return books
 
 
 # Retrieve the librarian for a library
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = library.librarian
+    return librarian
 
 
 if __name__ == "__main__":
