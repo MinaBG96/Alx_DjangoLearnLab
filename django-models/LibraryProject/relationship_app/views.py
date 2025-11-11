9bf95bd8faee48a7752e6cd316458b8a47467bab
MinaBG96 <minaboles96@gmail.com>
2025-11-11 16:51:11 +0200

    Django Views and URL Configuration v1.1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import  Library ,Book
 
 # --------------------------
 # Function-based view
