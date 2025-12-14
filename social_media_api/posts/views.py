805c35f915ac7731f6c494c5a0e9f47cb8a936a5
MinaBG96 <minaboles96@gmail.com>
2025-12-14 20:35:31 +0200

    Implementing User Follows and Feed Functionality v1.3

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,28 +1,3 @@
-# from rest_framework import viewsets, permissions, filters
-# from .models import Post, Comment
-# from .serializers import PostSerializer, CommentSerializer
-# from .permissions import IsOwnerOrReadOnly
-
-
-# class PostViewSet(viewsets.ModelViewSet):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-#     filter_backends = [filters.SearchFilter]
-#     search_fields = ['title', 'content']
-
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
-
-
-# class CommentViewSet(viewsets.ModelViewSet):
-#     queryset = Comment.objects.all()
-#     serializer_class = CommentSerializer
-#     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
@@ -34,13 +9,10 @@ class FeedView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        # ✅ checker عايز following.all()
         following_users = request.user.following.all()
 
-        # ✅ checker عايز السطر ده حرفيًا
-        posts = Post.objects.filter(
-            author__in=following_users
-        ).order_by('-created_at')
+        # ✅ السطر لازم يكون في سطر واحد
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
