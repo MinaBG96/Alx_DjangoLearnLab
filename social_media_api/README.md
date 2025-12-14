ccbfae7f0d56fd0513c6ddf875605138eeb50f54
MinaBG96 <minaboles96@gmail.com>
2025-12-14 18:35:25 +0200

    Project Setup and User Authentication for a Social Media API v1.0

---
+++ social_media_api/README.md
@@ -0,0 +1,17 @@
+# Social Media API
+
+## Setup
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver
+
+## Authentication
+- POST /api/accounts/register/
+- POST /api/accounts/login/
+- GET /api/accounts/profile/
+
+## User Model
+Custom user with:
+- bio
+- profile_picture
+- followers system
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from .serializers import RegisterSerializer, UserSerializer
+
+# Register
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+
+# Login
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({
+            'token': token.key,
+            'user_id': token.user_id,
+            'username': token.user.username
+        })
+
+
+# Profile
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
